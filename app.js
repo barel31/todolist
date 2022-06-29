c8f8e230d3ce14e2a130e8c8117aef39340d6207
Barel Shraga <barel123133@gmail.com>
2022-06-30 01:34:04 +0300

    Added default item

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const defaultItems = [
     Item({ name: 'Welcome to your todolist!' }),
     Item({ name: 'Hit the + button to add a new item.' }),
     Item({ name: '<-- Hit this to delete an item.' }),
-    Item({ name: 'Create custom lists with adding the name at the URL after \'/\'' }),
+    Item({ name: 'Create custom lists! Just add "/<listname>" to the end of the URL.' }),
 ];
 
 app.get('/', (req, res) => {
